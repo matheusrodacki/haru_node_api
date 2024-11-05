65952e5b12f958534f7adb69d888090030f51fd0
Matheus Rodacki <44122620+matheusrodacki@users.noreply.github.com>
2024-11-05 08:21:00 +0000

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Haru Node API')
+    .setDescription('API para gerenciar usuários e organizações')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
