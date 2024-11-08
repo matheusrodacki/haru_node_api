dcf6dfd9cf5b8290764f784c16fd62cce02564d1
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2024-11-08 11:57:39 -0300

    redireciona a rota principal para https://haru.digital e remove o interceptor de transformação

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    //redirect to https://haru.digital
+    return '<script>window.location.href="https://haru.digital";</script>';
   }
 }
