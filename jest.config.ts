35138e6f75cccd1fb6856ee4afc96d7748fa8702
Matheus Rodacki <44122620+matheusrodacki@users.noreply.github.com>
2024-11-08 08:49:02 +0000

    add users.controller.spec.ts

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+import { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: './src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': [
+      'ts-jest',
+      {
+        tsconfig: './tsconfig.json',
+      },
+    ],
+  },
+  // Add moduleNameMapper to handle path aliases
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/$1',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+};
+
+export default config;
