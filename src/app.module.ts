b29e00acf5402f30536852e01c4f0fb0646e5888
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2024-11-18 20:48:24 +0000

    restore functional db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,42 @@
 import { Module } from '@nestjs/common';
-import { AdminModule } from './database/admin.module';
-import { ClientModule } from './database/client.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './admin/users_admin/users.module';
+import { ClientsModule } from './admin/clients_tenants/clients.module';
+import { AuthModule } from './auth/auth.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { SeedService } from './seed.service';
+import { AddressesModule } from './admin/addresses_admin/addresses.module';
+import { CompaniesModule } from './admin/companies/companies.module';
+import { IndividualsModule } from './admin/individuals/individuals.module';
 
 @Module({
-  imports: [AdminModule, ClientModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('ADMIN_MYSQL_HOST'),
+        port: configService.get<number>('MYSQL_PORT'),
+        username: configService.get<string>('ADMIN_MYSQL_USER'),
+        password: configService.get<string>('ADMIN_MYSQL_PASSWORD'),
+        database: configService.get<string>('ADMIN_MYSQL_DATABASE'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
+    ClientsModule,
+    AuthModule,
+    CompaniesModule,
+    IndividualsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService, SeedService],
 })
 export class AppModule {}
--- src/database/admin.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DatabaseModule } from '../database/database.module';
-
-@Module({
-  imports: [DatabaseModule],
-  controllers: [], // Defina aqui os controllers do admin
-  providers: [], // Defina aqui os serviços do admin
-})
-export class AdminModule {}
